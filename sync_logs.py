e2e720962508b754a2797156772079d45b1a3f73
Sharon-Tseng <sharontseng8912@gmail.com>
2024-08-06 15:33:20 +0800

    Make the code efficient

--- sync_logs.py
+++ sync_logs.py
@@ -3,7 +3,6 @@ import openpyxl
 import datetime
 
 
-
 # using BCOMM as testing 
 # trustee_latest_df: to be updated, should be the latest indi trustee updates.xlsx
 trustee_latest_df = pd.read_excel(f"C:\\Users\\Sharon YY Tseng\\Desktop\\issue_log_automation\\Issue_log_automation-\\Model Office BComm Issue Log_20240729 (3).xlsx", 
@@ -32,42 +31,66 @@ detailed_tl_df.columns = ['Issue ID (TR_No.)', 'Raised by (Drop down list)', 'Su
                           'Proposed Workaround (if applicable) / Clarification Response','Simulation Re-run Result']
 
 
-# Save and drop "Sub-Issue ID" for future merge
-sub_issue_id = detailed_tl_df["Sub-Issue ID"]
+# drop "Sub-Issue ID" 
 detailed_tl_df = detailed_tl_df.drop("Sub-Issue ID", axis = "columns")
 
 
-# reset index as Issue ID (TR_No.)
-detailed_tl_df = detailed_tl_df.set_index("Issue ID (TR_No.)")
-trustee_latest_df = trustee_latest_df.set_index("Issue ID (TR_No.)")
+def get_updates(to_be_changed_df, accordance_df):
+    to_be_changed_df = to_be_changed_df.set_index("Issue ID (TR_No.)")
+    accordance_df = accordance_df.set_index("Issue ID (TR_No.)")
+    
+    
+    updates_df = pd.DataFrame(columns=["Issue ID","Column Name","Old Value","New Value"])
+    for issue_id in accordance_df.index:   #BCOMM_001~BCOMM200
+        if issue_id in to_be_changed_df.index:   #BCOMM_001~BCOMM143
+            for col in to_be_changed_df.columns:
+                if to_be_changed_df.at[issue_id, col] != accordance_df.at[issue_id,col]:
+                    new_row = {"Issue ID":issue_id, "Column Name":col,
+                               "Old Value": to_be_changed_df.at[issue_id, col],
+                               "New Value": accordance_df.at[issue_id, col]}
+                    updates_df.loc[len(updates_df)] = new_row
+            to_be_changed_df.loc[issue_id] = accordance_df.loc[issue_id]
 
 
-updates_df = pd.DataFrame(columns=["Issue ID","Column Name","Old Value","New Value"])
-for issue_id in trustee_latest_df.index:   #BCOMM_001~BCOMM200
-    if issue_id in detailed_tl_df.index:   #BCOMM_001~BCOMM143
-        for col in detailed_tl_df.columns:
-            if detailed_tl_df.at[issue_id, col] != trustee_latest_df.at[issue_id,col]:
-                new_row = {"Issue ID":issue_id, "Column Name":col,
-                           "Old Value": detailed_tl_df.at[issue_id, col],
-                           "New Value": trustee_latest_df.at[issue_id, col]}
-                updates_df.loc[len(updates_df)] = new_row
-        detailed_tl_df.loc[issue_id] = trustee_latest_df.loc[issue_id]
-
-
-    else:
-        new_row = trustee_latest_df.loc[issue_id]
-        for col in new_row.index:
-            detailed_tl_df.at[issue_id, col] = new_row[col]
-            detailed_tl_df.loc[issue_id] = trustee_latest_df.loc[issue_id]    
-
-detailed_tl_df.to_excel("test_detailed_tl.xlsx")
-trustee_latest_df.to_excel("test_trustee.xlsx")
+        else:
+            new_row = accordance_df.loc[issue_id]
+            for col in new_row.index:
+                to_be_changed_df.at[issue_id, col] = new_row[col]
+                to_be_changed_df.loc[issue_id] = accordance_df.loc[issue_id]
                 
-updates_df = updates_df.dropna(subset = ["Old Value", "New Value"], how="all")
+    return to_be_changed_df, accordance_df, updates_df    
+     
+result = get_updates(detailed_tl_df, trustee_latest_df)
+result[0].to_excel("test_detailed_tl.xlsx")
+result[1].to_excel("test_trustee.xlsx")
+                
+updates_df = result[2].dropna(subset = ["Old Value", "New Value"], how="all")
+
+updates_df= updates_df[updates_df['Column Name'].isin(["Issue Description Note: Please be reminded to exclude personal information in description."
+                                                        ,"Status (Drop down list)"])]
+
+print(updates_df)
 updates_df.to_excel("updates.xlsx", index=False)
 
-    
+# # reset index as Issue ID (TR_No.)
+# detailed_tl_df = detailed_tl_df.set_index("Issue ID (TR_No.)")
+# trustee_latest_df = trustee_latest_df.set_index("Issue ID (TR_No.)")
 
 
+# updates_df = pd.DataFrame(columns=["Issue ID","Column Name","Old Value","New Value"])
+# for issue_id in trustee_latest_df.index:   #BCOMM_001~BCOMM200
+#     if issue_id in detailed_tl_df.index:   #BCOMM_001~BCOMM143
+#         for col in detailed_tl_df.columns:
+#             if detailed_tl_df.at[issue_id, col] != trustee_latest_df.at[issue_id,col]:
+#                 new_row = {"Issue ID":issue_id, "Column Name":col,
+#                            "Old Value": detailed_tl_df.at[issue_id, col],
+#                            "New Value": trustee_latest_df.at[issue_id, col]}
+#                 updates_df.loc[len(updates_df)] = new_row
+#         detailed_tl_df.loc[issue_id] = trustee_latest_df.loc[issue_id]
 
 
+#     else:
+#         new_row = trustee_latest_df.loc[issue_id]
+#         for col in new_row.index:
+#             detailed_tl_df.at[issue_id, col] = new_row[col]
+#             detailed_tl_df.loc[issue_id] = trustee_latest_df.loc[issue_id]    
